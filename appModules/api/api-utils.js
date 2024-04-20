69f31fb70e3ba06aeb776ec42f6f00a94f7a2446
mcfaep <167646411+mcfaep@users.noreply.github.com>
2024-04-21 00:50:18 +0500

    initial commit

---
+++ appModules/api/api-utils.js
@@ -0,0 +1,16 @@
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+
+        return await response.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getRandomGame(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+module.exports = { getData, getRandomGame };
